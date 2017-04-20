d57694c19efcadad6d42d6a7157d828a87dd8c9d
Gan, Sze Kai <szekai.gan@sc.com>
2017-04-20 13:15:34 +0800

    Add Breeze to Saddle implicit

--- src/main/scala/sky/utils/ImplicitConversion.scala
+++ src/main/scala/sky/utils/ImplicitConversion.scala
@@ -15,4 +15,8 @@ object ImplicitConversion {
   implicit def mat2DenseMat[A:ClassTag](a: Mat[A])(implicit rl: LiteralRow[IndexedSeq[A],A], zero: Zero[A], man : ClassTag[A], ev:ST[A]):DenseMatrix[A]={
     DenseMatrix(a.cols.map(_.toSeq): _*).t
   }
+
+  implicit def densevec2vec[A:ClassTag](a: DenseVector[A]): Vec[A]= Vec(a.data)
+
+  implicit def denseMat2mat[A:ClassTag](a: DenseMatrix[A]): Mat[A] = Mat(a.cols,a.rows, a.data).transpose
 }
